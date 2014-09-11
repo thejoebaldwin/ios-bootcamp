0ead40717d0e17eb02b843ceec2c590c98c134a6
Joseph Baldwin <JoeBaldwin@Josephs-iMac.local>
2014-09-11 16:23:53 -0500

    More assignment4

--- Assignment4/Assignment4/ViewController.swift
+++ Assignment4/Assignment4/ViewController.swift
@@ -25,16 +25,56 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func AutoButtonClick(sender: AnyObject) {
+        
+        pickerView.selectRow( 6, inComponent: 0, animated: true)
+        pickerView.selectRow( 5, inComponent: 1, animated: true)
+        pickerView.selectRow( 4, inComponent: 2, animated: true)
+    }
+ 
+    @IBAction func TryButtonClick(sender: AnyObject) {
+        var label1 = pickerView.viewForRow(  pickerView.selectedRowInComponent(0), forComponent: 0) as? UILabel;
+        var label2 = pickerView.viewForRow(  pickerView.selectedRowInComponent(0), forComponent: 1) as? UILabel;
+        var label3 = pickerView.viewForRow(  pickerView.selectedRowInComponent(0), forComponent: 2) as? UILabel;
+        
+        if label1?.text == "6" && label2?.text == "5" && label3?.text == "4" {
+            println("match");
+        }
+        else {
+            println("no match");
+        }
+    }
 }
 
 extension ViewController: UIPickerViewDataSource {
 
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1;
+        return 3;
     }
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            return 1;
+            return 10;
+    }
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView!) -> UIView
+    {
+        var tempLabel:UILabel = UILabel();
+        
+        
+        
+        tempLabel.text = String(format: "%d", row);
+        tempLabel.center = CGPointMake(160, 284)
+        tempLabel.textAlignment = NSTextAlignment.Center
+        return tempLabel;
+
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView!, widthForComponent component: Int) -> CGFloat
+    {
+     
+        var value:CGFloat = 15;
+        return value;
     }
   
 }
